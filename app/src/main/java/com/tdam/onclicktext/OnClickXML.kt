9a835bf5ece6f69f44b6f915814cba6602618991
Manuel Prieto Macias <lolop_1993@hotmail.com>
2023-10-16 17:41:09 +0200

    onclickxml

---
+++ app/src/main/java/com/tdam/onclicktext/OnClickXML.kt
@@ -0,0 +1,24 @@
+package com.tdam.onclicktext
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+class OnClickXML : AppCompatActivity() {
+
+    private var contador = 0 //es un contador para el textview que se guarda con el estado de la actividad
+    private lateinit var textView: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_click_xml)
+
+        textView = findViewById(R.id.textView)//seteamos el textview de la clase con el textview del layout
+    }
+
+    fun incrementarTexto(view: View) {
+        contador++
+        textView.text = contador.toString()
+    }
+
+}
