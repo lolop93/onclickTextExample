58566ede4530b32d281fff300a41f29fd4938863
Manuel Prieto Macias <lolop_1993@hotmail.com>
2023-10-26 13:31:01 +0200

    onclickxml

--- app/src/main/java/com/tdam/onclicktext/MainActivity.kt
+++ app/src/main/java/com/tdam/onclicktext/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
         val textView = findViewById<TextView>(R.id.textView)
         val button = findViewById<Button>(R.id.button)
+
 
         button.setOnClickListener {
             // Cambiar el texto
--- app/src/main/java/com/tdam/onclicktext/OnClickXML.kt
+++ app/src/main/java/com/tdam/onclicktext/OnClickXML.kt
@@ -3,22 +3,27 @@ package com.tdam.onclicktext
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 
 class OnClickXML : AppCompatActivity() {
 
     private var contador = 0 //es un contador para el textview que se guarda con el estado de la actividad
     private lateinit var textView: TextView
+    private lateinit var imageView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_on_click_xml)
 
         textView = findViewById(R.id.textView)//seteamos el textview de la clase con el textview del layout
+        imageView = findViewById<ImageView>(R.id.imageView1)
+
     }
 
     fun incrementarTexto(view: View) {
         contador++
         textView.text = contador.toString()
+        imageView.setImageResource(R.drawable.fondo1_background)
     }
 
 }
